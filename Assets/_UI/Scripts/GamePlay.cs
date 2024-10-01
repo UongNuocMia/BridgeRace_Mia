43208845eca8c9e32d11c34b7ab75fddc6fae290
Mia <>
2024-10-02 01:25:24 +0700

    no message

--- Assets/_UI/Scripts/GamePlay.cs
+++ Assets/_UI/Scripts/GamePlay.cs
@@ -9,16 +9,4 @@ public class GamePlay : UICanvas
         UIManager.Ins.OpenUI<Setting>();
         GameManager.Ins.ChangeState(GameState.Setting);
     }
-
-    //public void WinButton()
-    //{
-    //    UIManager.Ins.OpenUI<Win>().score.text = Random.Range(100, 200).ToString();
-    //    Close(0);
-    //}
-
-    //public void LoseButton()
-    //{
-    //    UIManager.Ins.OpenUI<Lose>().score.text = Random.Range(0, 100).ToString(); 
-    //    Close(0);
-    //}
 }
